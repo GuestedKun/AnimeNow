7aae6dea6befd84edf53536ae17cb29aece34c00
Daniel Bady <badydaniel@seznam.cz>
2023-08-02 23:49:48 +0200

    fix tests

--- Tests/AnimeClientTests/AnimeClientTests.swift
+++ Tests/AnimeClientTests/AnimeClientTests.swift
@@ -6,7 +6,7 @@ final class AnimeClientTests: XCTestCase {
     func testFetchingAllEpisodeProviders() async throws {
         let animeClient = AnimeClient.testValue
 
-        let providers = try await animeClient.getAnimeProviders()
+        let providers = try await animeClient.getAnimeProviders(URL(string: "https://api.consumet.org").unsafelyUnwrapped)
 
         XCTAssertEqual(providers.count, 3)
     }
